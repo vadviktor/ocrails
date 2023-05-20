1bf7cdb396aa74823c3b93edd229b0f65248b363
Viktor (Ikon) VAD <git@vadviktor.xyz>
2023-05-20 16:36:00 +0100

    update project migration

--- db/migrate/20230520115816_create_projects.rb
+++ db/migrate/20230520115816_create_projects.rb
@@ -1,8 +1,8 @@
 class CreateProjects < ActiveRecord::Migration[7.0]
   def change
     create_table :projects do |t|
-      t.string :name
-      t.string :internal_id
+      t.string :name, null: false
+      t.string :internal_id, null: false
 
       t.timestamps
     end
