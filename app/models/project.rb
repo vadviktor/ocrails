a5be062df3849e691eb1d50d82161fe86805b016
Viktor (Ikon) VAD <git@vadviktor.xyz>
2023-05-27 16:52:32 +0100

    Nice new project validation with message

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   validates_presence_of :name, :internal_id
-  validates_uniqueness_of :internal_id
+  validates_uniqueness_of :internal_id, message: ->(object, data) { "Project name #{data[:value]} (or a very similar) is already taken." }
 
   before_validation :generate_internal_id
 
