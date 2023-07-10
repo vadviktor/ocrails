7f376ef69568c00ba5f6515961c16df7f3caeab9
Viktor (Ikon) VAD <git@vadviktor.xyz>
2023-07-10 19:05:30 +0100

    reworked the text list and added tooltip support

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,10 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const buttons = document.querySelectorAll("[data-text_select_id]");
     buttons.forEach((button) => {
-        button.addEventListener("click", function() {
+        button.addEventListener("click", function () {
             const textInputId = this.dataset.text_select_id;
             const inputElement = document.querySelector(`[data-text_input_id="${textInputId}"]`);
             inputElement.select();
         });
     });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 });
