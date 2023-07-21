746ebcf65f3e5e0fa443a3ac469b995ff4bda6b2
Viktor (Ikon) VAD <git@vadviktor.xyz>
2023-07-21 18:51:46 +0100

    working copy to clipboard even on dom replace

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,31 +1,26 @@
+// does not work on documents sidebar after reload
 document.addEventListener("DOMContentLoaded", function () {
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 });
 
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    const clipboard_buttons = document.querySelectorAll('[id^="clipboard-"]');
-
-    clipboard_buttons.forEach(button => {
-        button.addEventListener('click', (event) => {
-        let currentTarget = event.currentTarget;
-            navigator.permissions.query({name: "clipboard-write"}).then(result => {
-                if (result.state == "granted" || result.state == "prompt") {
-                    // Clipboard access is granted. Use it to write to the clipboard
-                const text = document.getElementById(`${currentTarget.id}-text`).innerText;
-                    try {
-                        navigator.clipboard.writeText(text);
-                        console.log('Text copied to clipboard');
-                    } catch (err) {
-                        console.log('Failed to copy text: ', err);
-                    }
-                } else {
-                    console.log('Clipboard access denied');
-                }
-            }).catch(err => {
-                console.log('Clipboard access denied: ', err);
-            });
-        });
-    })
-});
+let copyToClipboard = (e) => {
+    let currentTarget = e.currentTarget;
+    navigator.permissions.query({name: "clipboard-write"}).then(result => {
+        if (result.state == "granted" || result.state == "prompt") {
+            // Clipboard access is granted. Use it to write to the clipboard
+            const text = document.getElementById(`${currentTarget.id}-text`).innerText;
+            try {
+                navigator.clipboard.writeText(text);
+                console.log('Text copied to clipboard');
+            } catch (err) {
+                console.log('Failed to copy text: ', err);
+            }
+        } else {
+            console.log('Clipboard access denied');
+        }
+    }).catch(err => {
+        console.log('Clipboard access denied: ', err);
+    });
+}
