c91a8f45af967eb74a2462063acd13a71847fe70
Viktor (Ikon) VAD <git@vadviktor.xyz>
2023-07-25 19:39:04 +0100

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,70 +10,69 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_715_164_206) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_15_164206) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'active_storage_attachments', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'record_type', null: false
-    t.bigint 'record_id', null: false
-    t.bigint 'blob_id', null: false
-    t.datetime 'created_at', null: false
-    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
-    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness',
-                                                    unique: true
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table 'active_storage_blobs', force: :cascade do |t|
-    t.string 'key', null: false
-    t.string 'filename', null: false
-    t.string 'content_type'
-    t.text 'metadata'
-    t.string 'service_name', null: false
-    t.bigint 'byte_size', null: false
-    t.string 'checksum'
-    t.datetime 'created_at', null: false
-    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table 'active_storage_variant_records', force: :cascade do |t|
-    t.bigint 'blob_id', null: false
-    t.string 'variation_digest', null: false
-    t.index %w[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table 'images', force: :cascade do |t|
-    t.bigint 'project_id', null: false
-    t.boolean 'text_extracted', default: false, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'position'
-    t.index ['project_id'], name: 'index_images_on_project_id'
+  create_table "images", force: :cascade do |t|
+    t.bigint "project_id", null: false
+    t.boolean "text_extracted", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "position"
+    t.index ["project_id"], name: "index_images_on_project_id"
   end
 
-  create_table 'projects', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'internal_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['internal_id'], name: 'index_projects_on_internal_id', unique: true
-    t.index ['name'], name: 'index_projects_on_name', unique: true
+  create_table "projects", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "internal_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["internal_id"], name: "index_projects_on_internal_id", unique: true
+    t.index ["name"], name: "index_projects_on_name", unique: true
   end
 
-  create_table 'texts', force: :cascade do |t|
-    t.bigint 'image_id', null: false
-    t.text 'text', null: false
-    t.string 'svg_polygon_points', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'position'
-    t.boolean 'enabled', default: true
-    t.index ['image_id'], name: 'index_texts_on_image_id'
+  create_table "texts", force: :cascade do |t|
+    t.bigint "image_id", null: false
+    t.text "text", null: false
+    t.string "svg_polygon_points", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "position"
+    t.boolean "enabled", default: true, null: false
+    t.index ["image_id"], name: "index_texts_on_image_id"
   end
 
-  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'active_storage_variant_records', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'images', 'projects'
-  add_foreign_key 'texts', 'images'
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "images", "projects"
+  add_foreign_key "texts", "images"
 end
